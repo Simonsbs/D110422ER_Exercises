72c511fa57ab0e97a50b6b366659260461b5722a
Simon B.Stirling <simonsbs@gmail.com>
2023-06-19 18:11:12 +0300

    EX1 Solution 1

--- Exercises/EX2.cs
+++ Exercises/EX2.cs
@@ -5,6 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Exercises {
-	internal class EX2 {
+	public class EX2 {
+		public static bool CheckBrackets(string value) {
+			while (value.Contains("()") || 
+				value.Contains("{}") || 
+				value.Contains("[]")|| 
+				value.Contains("<>")) {
+
+				value = value.Replace("()", "");
+				value = value.Replace("{}", "");
+				value = value.Replace("[]", "");
+				value = value.Replace("<>", "");
+			}
+
+			return value.Length == 0;
+		}
 	}
 }
---
+++ Tests/TestsForEX2_CheckBrackets1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests {
+	public class TestsForEX2_CheckBrackets1 {
+		[Theory]
+		[InlineData("()")]
+		[InlineData("[]")]
+		[InlineData("(]", false)]
+		[InlineData("([<])", false)]
+		[InlineData("({[<>]})")]
+		public void TestsForEX2_CheckBrackets1_Check1(string valueToTest, bool expectedResult = true) {
+			//string valueToTest = "()";
+
+			bool result = Exercises.EX2.CheckBrackets(valueToTest);
+
+			Assert.Equal(result, expectedResult);
+		}
+	}
+}
