ea1ed06d506854dbad27903ea76036ffdc8555ed
Simon B.Stirling <simonsbs@gmail.com>
2023-06-26 18:30:26 +0300

    added ex4

---
+++ EX5.md
@@ -0,0 +1,10 @@
+#EX5
+
+1. Write a function
+2. find the longest common prefix for the array items
+3. if none found, return empty
+
+["floor", "flower", "flip"] - "fl"
+["floor", "flower"] - "flo"
+["cat", "dog", "frog", "donkey"] - ""
+["stream", "strong", "street"] - "str"
--- Tests/TestsForEX4_GetMaxProfit.cs
+++ Tests/TestsForEX4_GetMaxProfit.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Tests {
 	public class TestsForEX4_GetMaxProfit {
-		[Fact]
+		[Theory]
 		[InlineData(new[] { 8, 2, 5, 3, 6, 4 }, 4)]
 		[InlineData(new[] { 7, 6, 4, 3, 1 }, 0)]
 		public void TestsForEX4_GetMaxProfit_V1(int[] valueToTest, int expectedResult) {
