91e0d5dd9c5d32756f35d7232fcd36e4fbe6373f
Simon B.Stirling <simonsbs@gmail.com>
2023-07-10 19:19:38 +0300

    added solutions for EX8

---
+++ Exercises/LongestConsecutiveArray.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercises {
+	public static class LongestConsecutiveArray {
+
+		// [100,4,200,1,3,2]
+
+		public static int Solution1(int[] nums) {
+			int longestStreak = 0;
+
+			for (int i = 0; i < nums.Length; i++) {
+				int current = nums[i];
+				int streakLength = 1;
+
+				while (FoundInArray(nums, current + 1)) {
+					current += 1;
+					streakLength += 1;
+				}
+
+				longestStreak = Math.Max(longestStreak, streakLength);
+			}
+
+
+			return longestStreak;
+		}
+
+		private static bool FoundInArray(int[] nums, int valueToFind) {
+			for (int i = 0; i < nums.Length; i++) {
+				if (nums[i] == valueToFind) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+
+		public static int Solution2(int[] nums) {
+			var numSet = new HashSet<int>(nums);
+			int longestStreak = 0;
+
+			for (int i = 0; i < nums.Length; i++) {
+				int current = nums[i];
+				int streakLength = 1;
+
+				while (numSet.Contains(current + 1)) {
+					current += 1;
+					streakLength += 1;
+				}
+
+				longestStreak = Math.Max(longestStreak, streakLength);
+			}
+
+
+			return longestStreak;
+		}
+
+
+		public static int Solution3(int[] nums) {
+			int longestStreak = 0;
+
+			Dictionary<int, int> numSet = new Dictionary<int, int>();
+			
+
+			for (int i = 0; i < nums.Length; i++) {
+				int first = nums[i];
+				int last = nums[i];
+				int chain = 1;
+
+				if (numSet.ContainsKey(nums[i])) {
+					continue;
+				}
+
+				if (numSet.ContainsKey(nums[i] - 1)) {
+					chain += numSet[nums[i] - 1];
+					first = nums[i] - chain + 1;
+				}
+
+				if (numSet.ContainsKey(nums[i] + 1)) {
+					chain += numSet[nums[i] + 1];
+					last = first + chain - 1;
+				}
+
+				numSet[nums[i]] = 1;
+				numSet[first] = chain;
+				numSet[last] = chain;
+
+				longestStreak = Math.Max(longestStreak, chain);
+			}
+
+
+			return longestStreak;
+		}
+	}
+}
---
+++ Tests/TestsForExercises_LongestConsecutiveArray.cs
@@ -0,0 +1,30 @@
+﻿namespace Tests {
+	public class TestsForExercises_LongestConsecutiveArray {
+		[Theory]
+		[InlineData(new int[] { 100, 4, 200, 1, 3, 2 }, 4)]
+		[InlineData(new int[] { 0, 3, 7, 2, 5, 8, 4, 6, 0, 1 }, 9)]
+		public void TestsForExercises_LongestConsecutiveArray_V1(int[] nums, int expectedResult) {
+			long result = Exercises.LongestConsecutiveArray.Solution1(nums);
+
+			Assert.Equal(expectedResult, result);
+		}
+
+		[Theory]
+		[InlineData(new int[] { 100, 4, 200, 1, 3, 2 }, 4)]
+		[InlineData(new int[] { 0, 3, 7, 2, 5, 8, 4, 6, 0, 1 }, 9)]
+		public void TestsForExercises_LongestConsecutiveArray_V2(int[] nums, int expectedResult) {
+			long result = Exercises.LongestConsecutiveArray.Solution2(nums);
+
+			Assert.Equal(expectedResult, result);
+		}
+
+		[Theory]
+		[InlineData(new int[] { 100, 4, 200, 1, 3, 2 }, 4)]
+		[InlineData(new int[] { 0, 3, 7, 2, 5, 8, 4, 6, 0, 1 }, 9)]
+		public void TestsForExercises_LongestConsecutiveArray_V3(int[] nums, int expectedResult) {
+			long result = Exercises.LongestConsecutiveArray.Solution3(nums);
+
+			Assert.Equal(expectedResult, result);
+		}
+	}
+}
